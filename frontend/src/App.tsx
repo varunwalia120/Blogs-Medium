d4e47f9f48dd37c272c97c0fb13588d2637b6b7c
varunwalia120 <varunwalia608@gmail.com>
2026-02-07 23:07:09 +0530

    Fixed blog page routing bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,16 @@ import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
 import { Publish } from "./pages/Publish";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
